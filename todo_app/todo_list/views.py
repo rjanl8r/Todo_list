4cbdb14cbbb65af463b435d6babec195e8e656d2
Ramiz Jan <rjanl8r@gmail.com>
2020-03-26 16:00:42 -0700

    update

--- todo_app/todo_list/views.py
+++ todo_app/todo_list/views.py
@@ -41,4 +41,15 @@ def uncross(request, list_id):
     return redirect('home')
 
 
+def edit( request, list_id):
+    if request.method == 'POST':
+        item = List.objects.get(pk= list_id)
+        form = ListForm(request.POST or None, instance=item)
 
+        if form.is_valid():
+            form.save()
+            messages.success(request, ("Item Has Been Edited!"))
+            return redirect('home')
+    else:
+        item = List.objects.get(pk=list_id)  
+        return render(request, 'edit.html', {'item': item})
